a77dcf161e84521a59f58c9ed364f372e06a6e93
Faaiz <fffaaiz@gmail.com>
2025-03-09 09:07:40 +0100

    Added More Use Cases

--- tests/invite-friends-family.spec.ts
+++ tests/invite-friends-family.spec.ts
@@ -3,24 +3,20 @@ import player from 'play-sound';
 
 const play = player();
 
-test('Ummeldung / Abmeldung Termin', async ({ page }) => {
+test('Invite Friends and Family Termin', async ({ page }) => {
   await page.goto('https://terminvergabe.muelheim-ruhr.de/select2?md=9');
   await expect(page).toHaveTitle(/Terminvergabe/);
   await page.waitForSelector('#cookie_msg_btn_no', { timeout: 5000 });
   await page.click('#cookie_msg_btn_no');
 
-  await page.waitForSelector('#concerns_accordion-6953', { timeout: 5000 });
-  await page.click('#concerns_accordion-6953');
+  await page.waitForSelector('#concerns_accordion-6956', { timeout: 5000 });
+  await page.click('#concerns_accordion-6956');
 
-  await page.waitForSelector('#button-plus-2538', { timeout: 5000 });
-  await page.click('#button-plus-2538');
+  await page.waitForSelector('#button-plus-2574', { timeout: 5000 });
+  await page.click('#button-plus-2574');
 
   await page.waitForSelector('#WeiterButton', { timeout: 5000 });
   await page.click('#WeiterButton');
-
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[1]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[2]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[3]/div/label');
 
   await page.click('//*[@id="OKButton"]');
 
@@ -28,10 +24,10 @@ test('Ummeldung / Abmeldung Termin', async ({ page }) => {
   const nextTerminExists = await page.isVisible('text=Nächster Termin');
   expect(nextTerminExists).toBeTruthy();
   if (!nextTerminExists) {
-    console.log('Next Termin does not exist');
+    console.log('Next Termin for Invite Friends and Family does not exist');
   }
   else {
-    console.log('Next Termin for Ummeldung / Abmeldung exists');
+    console.log('Next Termin for Invite Friends and Family exists');
     const content = await page.textContent('//*[@id="suggest_location_content"]/form/dl/dd[4]');
     console.log('Date Time:', content);
     console.log("\u0007");
--- tests/request-pr-skilled-worker.spec.ts
+++ tests/request-pr-skilled-worker.spec.ts
@@ -3,17 +3,17 @@ import player from 'play-sound';
 
 const play = player();
 
-test('Ummeldung / Abmeldung Termin', async ({ page }) => {
+test('Settlement Permit for Skilled Workers Termin', async ({ page }) => {
   await page.goto('https://terminvergabe.muelheim-ruhr.de/select2?md=9');
   await expect(page).toHaveTitle(/Terminvergabe/);
   await page.waitForSelector('#cookie_msg_btn_no', { timeout: 5000 });
   await page.click('#cookie_msg_btn_no');
 
-  await page.waitForSelector('#concerns_accordion-6953', { timeout: 5000 });
-  await page.click('#concerns_accordion-6953');
+  await page.waitForSelector('#concerns_accordion-6956', { timeout: 5000 });
+  await page.click('#concerns_accordion-6956');
 
-  await page.waitForSelector('#button-plus-2538', { timeout: 5000 });
-  await page.click('#button-plus-2538');
+  await page.waitForSelector('#button-plus-2584', { timeout: 5000 });
+  await page.click('#button-plus-2584');
 
   await page.waitForSelector('#WeiterButton', { timeout: 5000 });
   await page.click('#WeiterButton');
@@ -21,6 +21,17 @@ test('Ummeldung / Abmeldung Termin', async ({ page }) => {
   await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[1]/div/label');
   await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[2]/div/label');
   await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[3]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[4]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[5]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[6]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[7]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[8]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[9]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[10]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[11]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[12]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[13]/div/label');
+  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[14]/div/label');
 
   await page.click('//*[@id="OKButton"]');
 
@@ -28,10 +39,10 @@ test('Ummeldung / Abmeldung Termin', async ({ page }) => {
   const nextTerminExists = await page.isVisible('text=Nächster Termin');
   expect(nextTerminExists).toBeTruthy();
   if (!nextTerminExists) {
-    console.log('Next Termin does not exist');
+    console.log('Next Termin for Settlement Permit does not exist');
   }
   else {
-    console.log('Next Termin for Ummeldung / Abmeldung exists');
+    console.log('Next Termin for Settlement Permit exists');
     const content = await page.textContent('//*[@id="suggest_location_content"]/form/dl/dd[4]');
     console.log('Date Time:', content);
     console.log("\u0007");
