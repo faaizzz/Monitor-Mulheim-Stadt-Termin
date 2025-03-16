ab203f72e0dd27c232a44437bc2cc33c9358f62c
Faaiz <fffaaiz@gmail.com>
2025-03-16 09:59:28 +0100

    Comment cookie while rerunning until succeeded

--- tests/extend-rp.spec.ts
+++ tests/extend-rp.spec.ts
@@ -6,8 +6,8 @@ const play = player();
 async function checkTermin(page) {
   await page.goto('https://terminvergabe.muelheim-ruhr.de/select2?md=9');
   await expect(page).toHaveTitle(/Terminvergabe/);
-  await page.waitForSelector('#cookie_msg_btn_no', { timeout: 5000 });
-  await page.click('#cookie_msg_btn_no');
+  // await page.waitForSelector('#cookie_msg_btn_no', { timeout: 5000 });
+  // await page.click('#cookie_msg_btn_no');
 
   await page.waitForSelector('#concerns_accordion-6956', { timeout: 5000 });
   await page.click('#concerns_accordion-6956');
@@ -18,19 +18,9 @@ async function checkTermin(page) {
   await page.waitForSelector('#WeiterButton', { timeout: 5000 });
   await page.click('#WeiterButton');
 
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[1]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[2]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[3]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[4]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[5]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[6]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[7]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[8]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[9]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[10]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[11]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[12]/div/label');
-  await page.click('//*[@id="TevisDialog"]/div/div/div[2]/div/div[13]/div/label');
+  for (let i = 1; i <= 13; i++) {
+    await page.click(`//*[@id="TevisDialog"]/div/div/div[2]/div/div[${i}]/div/label`);
+  }
 
   await page.click('//*[@id="OKButton"]');
 
